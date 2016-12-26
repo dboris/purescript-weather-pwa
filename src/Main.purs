7cfe30306e73aa20ae4990d036246989425d3ac3
Boris D <boris.dob@gmail.com>
2016-12-25 16:50:39 -0800

    Fix selector.

--- src/Main.purs
+++ src/Main.purs
@@ -70,7 +70,7 @@ updateForecastCard stateRef cardData =
       forcast = cardData.channel.item.forecast
   in do
     card <- getOrCreateCard stateRef key
-    find ".description" card >>= setText current.text
+    find "> .description" card >>= setText current.text
     find "> .date" card >>= setText current.date
     find ".current .icon" card >>= setClass (getIconClass current.code) true
     find ".current .temperature .value" card >>= (setText $ show current.temp)
@@ -87,7 +87,7 @@ updateForecastCard stateRef cardData =
 updateNextDay
   :: forall e
    . Tuple3 JQuery ForcastData WeekDay
-  -> Eff (console :: CONSOLE, dom :: DOM | e) Unit
+  -> Eff (dom :: DOM | e) Unit
 updateNextDay t =
   let c = get1 t
       daily = get2 t
@@ -169,36 +169,36 @@ getIconClass weatherCode =
 
 -- Fake data
 initialWeatherForecast :: WeatherCard
-initialWeatherForecast = {
-  key: "2459115"
-, label: "New York, NY"
-, created: "2016-07-22T01:00:00Z"
-, channel: {
-    astronomy: {
-      sunrise: "5:43 am"
-    , sunset: "8:21 pm"
-    }
-  , item: {
-      condition: {
-        text: "Windy"
-      , date: "Thu, 21 Jul 2016 09:00 PM EDT"
-      , temp: 56
-      , code: 24
+initialWeatherForecast =
+  { key: "2459115"
+  , label: "New York, NY"
+  , created: "2016-07-22T01:00:00Z"
+  , channel: {
+      astronomy: {
+        sunrise: "5:43 am"
+      , sunset: "8:21 pm"
       }
-    , forecast: [
-        {code: 44, high: 86, low: 70}
-      , {code: 44, high: 94, low: 73}
-      , {code: 4, high: 95, low: 78}
-      , {code: 24, high: 75, low: 89}
-      , {code: 24, high: 89, low: 77}
-      , {code: 44, high: 92, low: 79}
-      , {code: 44, high: 89, low: 77}
-      ]
-    }
-  , atmosphere: {humidity: 56}
-  , wind: {
-      speed: 25
-    , direction: 195
+    , item: {
+        condition: {
+          text: "Windy"
+        , date: "Thu, 21 Jul 2016 09:00 PM EDT"
+        , temp: 56
+        , code: 24
+        }
+      , forecast: [
+          {code: 44, high: 86, low: 70}
+        , {code: 44, high: 94, low: 73}
+        , {code: 4, high: 95, low: 78}
+        , {code: 24, high: 75, low: 89}
+        , {code: 24, high: 89, low: 77}
+        , {code: 44, high: 92, low: 79}
+        , {code: 44, high: 89, low: 77}
+        ]
+      }
+    , atmosphere: {humidity: 56}
+    , wind: {
+        speed: 25
+      , direction: 195
+      }
     }
   }
-}
