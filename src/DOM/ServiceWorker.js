9f8f753f2839345c9b866a9c7e3d5ae0d4131920
Boris D <boris.dob@gmail.com>
2017-01-07 18:33:16 -0800

    Wrap Eff action.

--- src/DOM/ServiceWorker.js
+++ src/DOM/ServiceWorker.js
@@ -1,13 +1,15 @@
 "use strict";
 
 exports.register_ = function (scriptURL, errback, callback) {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register(scriptURL)
-      .then(function (registration) {
-        callback(registration)();
-      });
-  }
-  else {
-    errback(new Error("serviceWorker API not supported"))();
-  }
+  return function () {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register(scriptURL)
+        .then(function (registration) {
+          callback(registration)();
+        });
+    }
+    else {
+      errback(new Error("serviceWorker API not supported"))();
+    }
+  };
 };
